3653d346ddf0e7fa5a3f841c3314a0937a4d5c20
Fabian Desnoes <fabiandes@hotmail.co.uk>
2020-09-05 09:47:47 +0100

    User setup

--- server/server.js
+++ server/server.js
@@ -1,36 +1,47 @@
 // Import the net library
 const net = require('net')
 
+// Import the User Object
+const User = require('./user')
+
 class Server {
-    constructor(motd="Welcome to the server!") {
+    constructor(motd = "Welcome to the server!") {
+        // Create set to store all users for broadcasting and logging
+        this.users = new Set()
+
         // Create server instance
         this.server = net.createServer((connection) => {
             console.log("New Connection Established @", connection.address)
 
             // When connection closes
-            connection.on('end', () => {
-                console.log("Connection Closed @", connection.address)
-            })
+            connection.on('end', () => console.log("Connection Closed @", connection.address))
 
-            connection.on('data', (msg) => {
-                console.log("Message recieved")
-                console.log(msg.toString())
-            })
+            // Parse incoming message
+            connection.on('data', this.parseMessage(data))
 
             // Send MOTD to client
-            connection.write(motd, (err) => {
-                if (err) {
-                    console.error("Error sending message to client.")
-                    console.log(err)
-                } else {
-                    console.log("MOTD sent to client.")
-                }
-            })
+            this.sendMessage(motd)
         })
     }
 
-    listen(port){
-        this.server.listen(port,() => {
+    sendMessage(msg) {
+        connection.write(msg, (err) => {
+            if (err) {
+                console.error("Error sending message to client.")
+                console.log(err)
+            } else {
+                console.log("MOTD sent to client.")
+            }
+        })
+    }
+
+    parseMessage(msg) {
+        console.log("Message recieved")
+        console.log(msg.toString())
+    }
+
+    listen(port) {
+        this.server.listen(port, () => {
             console.log('Server running on', this.server.address());
         });
     }
---
+++ server/user.js
@@ -0,0 +1,6 @@
+class User{
+    constructor(username, socket){
+        this.username = username
+        this.socket = socket
+    }
+}
